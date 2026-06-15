e558212310e0b819476754241a153058923cbf1a
Dewi Nurhalina Astadi <dewinurhalinaastadi222@gmail.com>
2026-06-15 18:46:37 +0700

    update fitur kirim pesan whatsapp otomatis

--- go.mod
+++ go.mod
@@ -8,7 +8,9 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.44
+	github.com/mdp/qrterminal/v3 v3.2.1
 	go.mau.fi/whatsmeow v0.0.0-20260603132417-6a7ac9915382
+	google.golang.org/protobuf v1.36.11
 	gorm.io/driver/mysql v1.6.0
 	gorm.io/gorm v1.31.1
 )
@@ -59,6 +61,7 @@ require (
 	golang.org/x/net v0.55.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
 	golang.org/x/sys v0.45.0 // indirect
+	golang.org/x/term v0.43.0 // indirect
 	golang.org/x/text v0.37.0 // indirect
-	google.golang.org/protobuf v1.36.11 // indirect
+	rsc.io/qr v0.2.0 // indirect
 )
--- whatsapp/wa.go
+++ whatsapp/wa.go
@@ -5,19 +5,74 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/mdp/qrterminal/v3"
 	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/proto/waE2E"
 	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types"
 	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
+	"google.golang.org/protobuf/proto"
 )
+
+var client *whatsmeow.Client
 
 func eventHandler(evt interface{}) {
 	switch v := evt.(type) {
 	case *events.Message:
 		fmt.Println("Received a message!", v.Message.GetConversation())
+		fmt.Println("=> dari saya =", v.Info.IsFromMe)
+		fmt.Println("=> pesan =", v.Message.GetConversation())
+		fmt.Println("=> Server =", v.Info.MessageSource.Chat.Server)
+		fmt.Println("=> apakah group = ", v.Info.IsGroup)
+		fmt.Println("=> apakah broadcast =", v.Info.IsIncomingBroadcast())
+
+		if !v.Info.IsFromMe &&
+			!v.Info.IsGroup &&
+			!v.Info.IsIncomingBroadcast() {
+
+			fmt.Println("PENGIRIM =", v.Info.Sender.User)
+			pesan := v.Message.GetConversation()
+			fmt.Println("PESAN ASLI =[" + pesan + "]")
+
+			// Normalisasi: lowercase dan trim spasi
+			pesanNormalized := strings.ToLower(strings.TrimSpace(pesan))
+			fmt.Println("PESAN NORMALIZED =[" + pesanNormalized + "]")
+
+			var id_wa []string
+			id_wa = append(id_wa, v.Info.ID)
+
+			client.MarkRead(context.Background(), id_wa, time.Now(), v.Info.Chat, v.Info.Sender)
+
+			client.SubscribePresence(context.Background(), v.Info.Sender)
+
+			client.SendPresence(context.Background(), types.PresenceAvailable)
+
+			time.Sleep(2 * time.Second)
+
+			client.SendChatPresence(context.Background(), v.Info.Sender, types.ChatPresenceComposing, types.ChatPresenceMediaText)
+
+			time.Sleep(3 * time.Second)
+
+			client.SendChatPresence(context.Background(), v.Info.Sender, types.ChatPresencePaused, types.ChatPresenceMediaText)
+
+			// STRICT CHECK: Hanya test atau tes
+			fmt.Println("=== CEK KONDISI ===")
+			fmt.Println("pesan == 'test': ", pesanNormalized == "test")
+			fmt.Println("pesan == 'tes': ", pesanNormalized == "tes")
+
+			if pesanNormalized == "test" || pesanNormalized == "tes" {
+				fmt.Println(">>> MEMBALAS PESAN <<<")
+				kirimPesan(v.Info.Sender)
+			} else {
+				fmt.Println(">>> TIDAK MEMBALAS (bukan test/tes) <<<")
+			}
+		}
 	}
 }
 
@@ -33,7 +88,7 @@ func KonekWa() {
 		panic(err)
 	}
 	clientLog := waLog.Stdout("Client", "DEBUG", true)
-	client := whatsmeow.NewClient(deviceStore, clientLog)
+	client = whatsmeow.NewClient(deviceStore, clientLog)
 	client.AddEventHandler(eventHandler)
 
 	if client.Store.ID == nil {
@@ -44,7 +99,7 @@ func KonekWa() {
 		}
 		for evt := range qrChan {
 			if evt.Event == "code" {
-				fmt.Println("QR code:", evt.Code)
+				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
 			} else {
 				fmt.Println("Login event:", evt.Event)
 			}
@@ -61,4 +116,20 @@ func KonekWa() {
 	<-c
 
 	client.Disconnect()
+}
+
+func kirimPesan(JIDPenerima types.JID) {
+	JIDPenerima.Device = 0
+	_, err := client.SendMessage(
+		context.Background(),
+		JIDPenerima,
+		&waE2E.Message{
+			Conversation: proto.String("UJI COBA\n PESAN OTOMATIS"),
+		},
+	)
+	if err != nil {
+		fmt.Println("ERROR kirim pesan:", err)
+	} else {
+		fmt.Println("Pesan berhasil terkirim!")
+	}
 }
